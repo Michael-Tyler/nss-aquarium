f2b71aab687f2d5c23eb156067f0dd8713b7d075
Michael-Tyler <miketylerabc@gmail.com>
2020-07-24 11:55:29 -0500

    used map method on locations

--- scripts/locations/LocationList.js
+++ scripts/locations/LocationList.js
@@ -4,13 +4,9 @@ import { locationAsHtml } from "../locations/Location.js"
 export const locationList = () => {
     const contentElement = document.querySelector(".content--left")
     const allTheLocations = useLocation()
-    let locationHtmlRepresentation = ""
-    for (const location of allTheLocations) {
-        locationHtmlRepresentation += locationAsHtml(location)
-    }
     contentElement.innerHTML += `
-    <article class="locations">
-    ${locationHtmlRepresentation}
-    </article>
+        <article class="locations">
+            ${allTheLocations.map(location => locationAsHtml(location))}
+        </article>
     `
 }
